72384e0989c8e0425080e4a58df62b2f21fa6b85
Felipe <felipe.lubra@gmail.com>
2021-09-06 17:59:09 -0300

    refactor: get accessToken from SpotifyService

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,19 +7,19 @@ use App\Services\SpotifyService;
 class DashboardController extends Controller
 {
     function index(SpotifyService $spotifyService) {
-        $accessToken = cache("spotify_access_token");
-        if (empty($accessToken) || $accessToken->hasExpired()) {
+        $accessToken = $spotifyService->getAccessToken();
+        if (empty($accessToken)) {
             return redirect('/connect/spotify');
         }
 
-        $lastTracks = cache("spotify_last_200_tracks", function() use ($spotifyService, $accessToken) {
-            $tracks = $spotifyService->getLastTracks($accessToken, 200);
+        $lastTracks = cache("spotify_last_200_tracks", function() use ($spotifyService) {
+            $tracks = $spotifyService->getLastTracks(200);
             cache()->put("spotify_last_200_tracks", $tracks, now()->addHour(1));
             return $tracks;
         });
 
-        $profile = cache("spotify_profile", function() use($spotifyService, $accessToken) {
-            $profile = $spotifyService->getUserProfile($accessToken);
+        $profile = cache("spotify_profile", function() use($spotifyService) {
+            $profile = $spotifyService->getUserProfile();
             cache()->put("spotify_profile", $profile, now()->addDay(7));
             return $profile;
         });
